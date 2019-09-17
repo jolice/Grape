23c83bbd10d2c47c728accc1bb2f712b978f2eeb
Vladimir Alexeev <25826296+riguron@users.noreply.github.com>
2019-09-17 22:28:25 +0300

    Test fixes

--- src/test/java/me/riguron/grape/loader/ClassListBeanDefinitionLoaderTest.java
+++ src/test/java/me/riguron/grape/loader/ClassListBeanDefinitionLoaderTest.java
@@ -7,14 +7,19 @@ import org.junit.jupiter.api.Test;
 import javax.inject.Inject;
 import java.util.Arrays;
 import java.util.HashSet;
-import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class ClassListBeanDefinitionLoaderTest {
 
+
     @Test
     void load() {
+
 
         ClassListBeanDefinitionLoader classListBeanDefinitionLoader =
                 new ClassListBeanDefinitionLoader(
@@ -23,27 +28,31 @@ class ClassListBeanDefinitionLoaderTest {
                 );
 
 
-        List<BeanDefinition> definitions = classListBeanDefinitionLoader.load();
+        Map<Class<?>, BeanDefinition> definitions = classListBeanDefinitionLoader.load().stream().collect(Collectors.toMap(BeanDefinition::getBeanClass, Function.identity()));
 
         assertEquals(3, definitions.size());
 
-        final BeanDefinition first = definitions.get(0);
+        final BeanDefinition first = definitions.get(B.class);
 
+        assertNotNull(first);
         assertEquals(B.class, first.getBeanClass());
         assertEquals(1, first.getConstructor().getDependencies().size());
 
 
-        final BeanDefinition second = definitions.get(1);
+        final BeanDefinition second = definitions.get(C.class);
 
+        assertNotNull(second);
         assertEquals(C.class, second.getBeanClass());
         assertEquals(2, second.getConstructor().getDependencies().size());
 
-        final BeanDefinition third = definitions.get(2);
+        final BeanDefinition third = definitions.get(A.class);
 
+        assertNotNull(third);
         assertEquals(A.class, third.getBeanClass());
         assertEquals(0, third.getConstructor().getDependencies().size());
 
     }
+
 
 
     public static class A {
--- src/test/java/me/riguron/grape/loader/ConfigurationBeanDefinitionLoaderTest.java
+++ src/test/java/me/riguron/grape/loader/ConfigurationBeanDefinitionLoaderTest.java
@@ -7,9 +7,12 @@ import org.junit.jupiter.api.Test;
 
 import javax.enterprise.inject.Produces;
 import java.util.Collections;
-import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class ConfigurationBeanDefinitionLoaderTest {
 
@@ -20,21 +23,23 @@ class ConfigurationBeanDefinitionLoaderTest {
                 new MethodInvoker(), Collections.singleton(new TestConfig())
         );
 
-        List<BeanDefinition> result = beanDefinitionLoader.load();
+        Map<Class<?>, BeanDefinition> definitions = beanDefinitionLoader.load().stream().collect(Collectors.toMap(BeanDefinition::getBeanClass, Function.identity()));
 
         assertEquals(2,
-                result.size());
+                definitions.size());
 
 
-        BeanDefinition first = result.get(1);
+        BeanDefinition first = definitions.get(String.class);
 
+        assertNotNull(first);
         assertEquals(String.class, first.getBeanClass());
 
         assertEquals(1, first.getConstructor().getDependencies().size());
         assertEquals(Integer.class, first.getConstructor().getDependencies().get(0).getType());
 
-        BeanDefinition second = result.get(0);
+        BeanDefinition second = definitions.get(Double.class);
 
+        assertNotNull(second);
         assertEquals(Double.class, second.getBeanClass());
 
         assertEquals(1, second.getConstructor().getDependencies().size());
